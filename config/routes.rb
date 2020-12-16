ea4ef4deab72c6f744e96e909461ecedb825fa48
Gene <gorange310@gmail.com>
2020-12-16 16:37:03 +0800

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :cards
   root 'cards#index'
+
+  resources :cards do
+    resources :comments, only: [:create]
+  end
+
+  resources :comments, only: [:edit, :update, :destroy]
   # get "/cards/new", to: "cards#new"
   # get "/cards", to: "cards#index"
 
