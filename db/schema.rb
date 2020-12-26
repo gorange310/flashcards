b0f946de98f771e653121bf230863536d1f8b073
Gene <gorange310@gmail.com>
2020-12-26 15:57:43 +0800

    rails g delayed_job:active_record

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_19_082544) do
+ActiveRecord::Schema.define(version: 2020_12_26_075622) do
 
   create_table "cards", force: :cascade do |t|
     t.string "title"
@@ -28,6 +28,21 @@ ActiveRecord::Schema.define(version: 2020_12_19_082544) do
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "deleted_at"
     t.index ["card_id"], name: "index_comments_on_card_id"
+  end
+
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer "priority", default: 0, null: false
+    t.integer "attempts", default: 0, null: false
+    t.text "handler", null: false
+    t.text "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string "locked_by"
+    t.string "queue"
+    t.datetime "created_at", precision: 6
+    t.datetime "updated_at", precision: 6
+    t.index ["priority", "run_at"], name: "delayed_jobs_priority"
   end
 
   create_table "favorite_cards", force: :cascade do |t|
